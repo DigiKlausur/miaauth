0093d3af0b3a8839cb999da56791e451c6a60491
Tim Metzler <33530562+tmetzl@users.noreply.github.com>
2023-04-05 10:26:18 +0000

    Add missing )

--- miaauth/auth.py
+++ miaauth/auth.py
@@ -3,7 +3,7 @@ from oauthenticator.generic import GenericOAuthenticator
 class MIAAuth(GenericOAuthenticator):
     
     def normalize_username(self, username):
-        self.log.debug(f"[MIAAuth] In normalize username. Username = {username}"
+        self.log.debug(f"[MIAAuth] In normalize username. Username = {username}")
         if isinstance(username, list):
             username = username[0]
         return super().normalize_username(username)
